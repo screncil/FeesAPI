82f747296fa9265d0b5e63042978f852e15ada01
screncil <alexplay1998s3@gmail.com>
2024-05-30 13:06:42 +0300

    Added Scraper

---
+++ fees/scraper/__init__.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup as BS
+
+from pprint import pprint
+
+
+
+class FeesScraper:
+
+    @property
+    def __getBS(self) -> BS:
+        r = requests.get("https://vyshybanky.com/")
+        return BS(r.content, "lxml")
+    
+    def getAll(self) -> list:
+        lst = []
+        all_cards = self.__getBS.find_all('div', {"class": "jars__item"})
+
+        for card in all_cards:
+            lst.append({
+                "donate_link": card.find("a", {"class": "jars-item__btn--donate"}).get("href").split("?t=")[0],
+                "purpose": card.find("h6", {"class": "jars-item__title"}).text,
+                "type": card.find("div", {"class": "jars-item__tag"}).get("title"),
+                "details_link": card.find("div", {"class": "jars-item__descr"}).find_next("a").get("href"),
+                "fees_creator": card.find("div", {"class": "jars-item__socials"}).find_next("a").get("href")
+            })
+
+        return lst
