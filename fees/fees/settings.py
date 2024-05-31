dbeec11eadc5491dd1784a93dfaded13f6b0f241
screncil <alexplay1998s3@gmail.com>
2024-05-31 12:13:57 +0300

    Changed debug to False

--- fees/fees/settings.py
+++ fees/fees/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uf_9x8%5g$1kcgcbu-w2fr4p^b8j_tppm$6k9%v35x6tdew$(d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
